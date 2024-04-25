27b24c1dae3f881669082a0843f2a700add69ed1
Hallex <136874674+Hallexz@users.noreply.github.com>
2024-04-25 18:50:33 +0700

    Update pipeline.py

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,7 +1,7 @@
 from sklearn.pipeline import Pipeline
 
-from src.MLP import FeedForwardNN
-from src.EDA import ProcessData, find_dataset, DataPipeline
+from MLP import FeedForwardNN
+from EDA import ProcessData, find_dataset, DataPipeline
 
 
 
